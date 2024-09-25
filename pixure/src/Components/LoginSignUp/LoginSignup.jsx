014653782c05dcaba57166a20875484222caa46f
sldang <spencerdang727@gmail.com>
2024-09-25 12:24:27 -0700

    added additional fields for sign up

--- pixure/src/Components/LoginSignUp/LoginSignup.jsx
+++ pixure/src/Components/LoginSignUp/LoginSignup.jsx
@@ -12,15 +12,40 @@ const LoginSignup = () => {
             <div className="underline"></div>
         </div>
         <div className="inputs">
-            {action==="Login"?<div></div>: <div className="input">
-                <input type="text" placeholder='Name'/>
-            </div>}
+            {
+            action==="Login"?<div></div>: 
+            <div className="input">
+                <input type="text" placeholder='First Name'/>
+            </div>
+            
+            }
+            {
+            action==="Login"?<div></div>: 
+            <div className="input">
+                <input type="text" placeholder='Last Name'/>
+            </div>
+            
+            }
+             {
+            action==="Login"?<div></div>: 
+            <div className="input">
+                <input type="text" placeholder='Username'/>
+            </div>
+            
+            }
             <div className="input">
                 <input type="email" placeholder='Email'/>
             </div>
             <div className="input">
                 <input type="password" placeholder='Password'/>
             </div>
+            {
+            action==="Login"?<div></div>: 
+            <div className="input">
+                <input type="number" placeholder='Zip Code'/>
+            </div>
+            
+            }
         </div>
         {action==="Sign Up"?<div></div>:<div className="forgot_password">Lost Password? <span>Click Here</span></div>}
         <div className="submit-container">
